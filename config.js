104641018485e252add73f055527bc029d733961
xNiskata <120335556+xNiskata@users.noreply.github.com>
2022-12-22 13:19:47 +0700

    Add files via upload
    KAMU NANYA KENAPA INI GW UPLOAD !?

---
+++ config.js
@@ -0,0 +1,52 @@
+const fs = require('fs')
+const chalk = require('chalk')
+
+//api
+global.xteam = 'apivproject'
+
+//stick
+global.author = 'Follow Instagram\n  @Kurokochi_  '
+global.packname = '6285801284666\n'
+
+//Link
+global.gc = 'https://chat.whatsapp.com/LoOyCcZuhEqIwjbqfrqJtg'
+global.linkyt = 'https://tiktok.com/@xkurokochi_'
+global.linkgc = 'https://chat.whatsapp.com/LoOyCcZuhEqIwjbqfrqJtg'
+
+// Othr
+global.owner = ['6282327759039']
+global.premium = ['0']
+global.ownername = 'Niskata'
+global.botname = 'Kon'
+global.wm = '© Niskata'
+global.footer = 'ꜱɪᴍᴘʟᴇ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ ᴍᴀᴅᴇ ʙʏ ɴɪꜱᴋᴀᴛᴀ ッ'
+global.sessionName = 'session'
+global.prefa = ['','!','.','🐦','🐤','🗿']
+global.sp = '∘'
+global.mess = {
+    success: 'Berhasil',
+    eror: 'Error',
+    admin: 'Fitur Khusus Admin Group!',
+    botAdmin: 'Bot Harus Menjadi Admin Terlebih Dahulu!',
+    owner: 'Fitur Khusus Owner Bot',
+    group: 'Fitur Digunakan Hanya Untuk Group!',
+    private: 'Fitur Digunakan Hanya Untuk Private Chat!',
+    bot: 'Fitur Khusus Pengguna Nomor Bot',
+    wait: 'Tunggu sebentar, sedang di proses',
+    endLimit: 'Limit Harian Anda Telah Habis, Limit Akan Direset Setiap Jam 12',
+}
+global.limitawal = {
+    premium: "Infinity",
+    free: 50
+}
+
+//gambar
+global.thumb = 'https://telegra.ph/file/86af4bca3faf0f9896d50.jpg'
+
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+})
